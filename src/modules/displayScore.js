afc09cb85929f3d3ed763947e80ecc36fd08d272
FevenSeyfu <fevenseyfu00@gmail.com>
2023-02-08 01:29:33 +0300

    Remove unnecessary code

--- src/modules/displayScore.js
+++ src/modules/displayScore.js
@@ -1,7 +1,6 @@
 import ScoreList from './createList.js';
 
 const displayScore = (scoreTable) => {
-  console.log(scoreTable);
   ScoreList.forEach((score) => {
     const newRow = document.createElement('tr');
     newRow.innerHTML = `
